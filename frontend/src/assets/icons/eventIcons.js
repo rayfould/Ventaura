8f07393067d032fb6c6a6853bbcd50cd4087527a
rudolfr <rudolfwrissling@gmail.com>
2024-12-09 00:50:23 -0500

    Updated Landing Page style
    Updated landing page styling size to cut back on the zoom, and make the elements less dominant

--- frontend/src/assets/icons/eventIcons.js
+++ frontend/src/assets/icons/eventIcons.js
@@ -1,14 +1,68 @@
 // src/assets/icons/eventIcons.js
-import VisualArts from './visualArts.png';
-import PerformingArts from './performingArts.png';
+import Baseball from './baseball.png';
 import Basketball from './basketball.png';
+import Community from './community.png';
+import Conferences from './conferences.png';
+import Exhibitions from './exhibitions.png';
+import Family from './family.png';
+import Festival from './festival.png';
+import Film from './film.png';
+import Fishing from './fishing.png';
+import FoodAndDrinks from './foodAndDrinks.png';
 import Football from './football.png';
+import Gaming from './gaming.png';
+import Hiking from './hiking.png';
+import Hockey from './hockey.png';
+import Holiday from './holiday.png';
+import Music from './music.png';
+import Networking from './networking.png';
+import Nightlife from './nightlife.png';
+import Other from './Other.png';
+import Outdoors from './outdoors.png';
+import PerformingArts from './performingArts.png';
+import Pets from './pets.png';
+import Pottery from './pottery.png';
+import Science from './science.png';
+import Soccer from './soccer.png';
+import Tennis from './tennis.png';
+import Theatre from './theatre.png';
+import Virtual from './virtual.png';
+import VisualArts from './visualArts.png';
+import Wellness from './wellness.png';
+import Workshops from './workshops.png';
 
 const eventIcons = {
-  'Visual Arts': VisualArts,
-  'Performing Arts': PerformingArts,
+  'Baseball': Baseball,
   'Basketball': Basketball,
+  'Community': Community,
+  'Conferences': Conferences,
+  'Exhibitions': Exhibitions,
+  'Family': Family,
+  'Festivals': Festival,
+  'Film': Film,
+  'Fishing': Fishing,
+  'Food and Drink': FoodAndDrinks,
   'Football': Football,
+  'Gaming': Gaming,
+  'Hiking': Hiking,
+  'Hockey': Hockey,
+  'Holiday': Holiday,
+  'Music': Music,
+  'Networking': Networking,
+  'Nightlife': Nightlife,
+  'Other': Other,
+  'Outdoors': Outdoors,
+  'Performing Arts': PerformingArts,
+  'Pets': Pets,
+  'Pottery': Pottery,
+  'Science': Science,
+  'Soccer': Soccer,
+  'Tennis': Tennis,
+  'Theater': Theatre,
+  'Virtual': Virtual,
+  'Visual Arts': VisualArts,
+  'Wellness': Wellness,
+  'Workshops': Workshops
 };
 
 export default eventIcons;
